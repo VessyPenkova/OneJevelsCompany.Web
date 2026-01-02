38e5ddffa095c800fb7ccf372845cc5bfcc9a891
vessy.penkova <vespenkova123@gmail.com>
2026-01-02 13:02:00 +0200

    Edited Collection Redirections Bug

--- Services/Payment/PaymentIntent.cs
+++ Services/Payment/PaymentIntent.cs
@@ -5,7 +5,7 @@
         public string Id { get; set; } = string.Empty;
         public string ClientSecret { get; set; } = string.Empty;
         public long AmountInCents { get; set; }
-        public string Currency { get; set; } = "usd";
+        public string Currency { get; set; } = "EUR";
         public string Status { get; set; } = "requires_payment_method";
     }
 }
